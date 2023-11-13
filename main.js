dd63468a11d6aa8b32df4996aa2137ffdfc48f35
brunot12 <110491548+brunot12@users.noreply.github.com>
2023-11-13 17:12:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert('Hello World!');
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();   
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+let contador = 0;
+//Estrutura de repetição - Enquanto
+while(contador<9){
+    listaDeTeclas[0].onclick = tocaSom;
+    contador = contador + 1;
+}
+
